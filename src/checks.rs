086c55948729010504c67dc35fe995d9fadcd85f
faruk <faruk.omer.erdem@proton.me>
2026-01-25 11:08:48 +0300

    ♻️ refactor: replace io::Result with anyhow::Result in error handling
    Migrate the error-handling mechanism in  and  to use anyhow for better flexibility and maintainability. Removed direct process exits and improved error propagation using bail and context.

--- src/checks.rs
+++ src/checks.rs
@@ -1,32 +1,21 @@
 use heim_virt;
-use std::io;
+use anyhow::{Result, Context, bail};
 
-pub async fn check_virtualization() -> io::Result<()> {
+pub async fn check_virtualization() -> Result<()> {
     let virtualization = heim_virt::detect()
         .await
-        .expect("Failed to detect virtualization");
+        .context("Failed to detect virtualization")?;
     if virtualization == heim_virt::Virtualization::Lxc {
-        eprintln!(
-            r#"
-        LXC is not supported (yet).
-        WireGuard can technically run in an LXC container,
-        but the kernel module has to be installed on the host,
-        the container has to be run with some specific parameters
-        and only the tools need to be installed in the container.
-        "#
-        );
-        std::process::exit(1);
+        bail!("LXC is not supported. Refer to documentation for details.");
     } else if virtualization == heim_virt::Virtualization::OpenVz {
-        eprintln!("OpenVZ is not supported");
-        std::process::exit(1);
+        bail!("OpenVZ is not supported.");
     }
     Ok(())
 }
 
-pub fn is_root() -> io::Result<()> {
+pub fn is_root() -> Result<()> {
     if unsafe { libc::getuid() } != 0 {
-        eprintln!("You must be root to run this installer.");
-        std::process::exit(1);
+        bail!("You must be root to run this installer.");
     };
     Ok(())
 }
